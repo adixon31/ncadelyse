57518af40d379eb5608337e4bfb79b983e53a252
Angel Dixon <angel.dixon31@gmail.com>
2016-02-28 20:39:23 -0500

    Updates UI

--- js/MainController.js
+++ js/MainController.js
@@ -1,7 +1,7 @@
 app.controller('MainController', ['$scope', '$location', function($scope, $location){
   $scope.title = "Portfolio";
   $scope.promo = "Send all inquiries to angel.dixon31@gmail.com.";
-  $scope.about = "One year ago, I graduated with a Bachelor's of Science in Computational Media Student from the Georgia Institute of Technology. My studies involved computer programming, design, and understanding the computer as a medium. While my more personal interests include film, narrative, and animation, I am open to experience other branches of my field such as UI/UX Design, game development, marketing, and web development./nI am currently a software developer at a company that provides conferencing and collaboration solutions."
+  $scope.about = "One year ago, I graduated with a Bachelor's of Science in Computational Media Student from the Georgia Institute of Technology. My studies involved computer programming, design, and understanding the computer as a medium. While my more personal interests include film, narrative, and animation, I am open to experience other branches of my field such as UI/UX Design, game development, marketing, and web development.\nI am currently a software developer at a company that provides conferencing and collaboration solutions."
   $scope.products = [
     {
       name: 'Program or Be Programmed',
